8680e7585d3e68f82f0c8f2ad5b1442d56e0b047
TonyMax <antoshidza@mail.ru>
2023-05-19 13:33:35 +0300

    Sorting update
    Add better sorting authoring workflow for future use.
    
    Current sorting authoring is just a dumb index assigning, so whenever user decide to change layers relevant positions then all indices in all affected prefabs should be changed accordingly. In unity built-in components like `SpriteRenderer` there is a field `SortingLayer` which appears as dropdown selection and reference sorting layers defined in project. Moreover it's synced with all changes user make to sorting layers list like changing layer name or position in list.
    
    Added workflow unlocks using unity's sorting list by injecting layer id into `int` field by using `[SortingLayer]` attribute. Later it can be used to retrieve layer index like `SortingLayer.GetLayerValueFromID(_layerID)`.

---
+++ Sorting/Editor/SortingLayerPropertyDrawer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+namespace NSprites.Editor
+{
+    [CustomPropertyDrawer(typeof(SortingLayerAttribute))]
+    public class SortingLayerPropertyDrawer : PropertyDrawer
+    {
+        private const string SortingLayerFieldMethodName = "SortingLayerField";
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer && property.propertyType != SerializedPropertyType.String)
+                Debug.LogError("SortedLayer property should be an integer or string ( the layer id )");
+            else
+                SortingLayerField(new GUIContent("Sorting Layer"), property, EditorStyles.popup, EditorStyles.label);
+        }
+
+        public static void SortingLayerField(GUIContent label, SerializedProperty layerID, GUIStyle style, GUIStyle labelStyle)
+        {
+            var methodInfo = typeof(EditorGUILayout).GetMethod(SortingLayerFieldMethodName, BindingFlags.Static | BindingFlags.NonPublic, null, new[] { typeof(GUIContent), typeof(SerializedProperty), typeof(GUIStyle), typeof(GUIStyle) }, null);
+
+            if (methodInfo == null)
+            {
+                Debug.LogWarning($"{nameof(SortingLayerPropertyDrawer)} can't find {SortingLayerFieldMethodName}.");
+                return;
+            }
+                
+            var parameters = new object[] { label, layerID, style, labelStyle };
+            methodInfo.Invoke(null, parameters);
+        }       
+    }
+}
