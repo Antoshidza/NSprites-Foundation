c4651947c95ed87717eaf78836a3b94ffcccdfbf
TonyMax <antoshidza@mail.ru>
2023-06-05 10:46:54 +0300

    Add `GetSize` sprite method to `Utils` class

---
+++ Base/Common/Utils.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace NSprites
+{
+    public static class Utils
+    {
+        public static float2 GetSize(this Sprite sprite) => new(sprite.bounds.size.x, sprite.bounds.size.y);
+    }
+}
