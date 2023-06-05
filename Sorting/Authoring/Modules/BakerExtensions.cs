23e651e4e8098a9f228a9e8641af3a0b0f53167d
TonyMax <antoshidza@mail.ru>
2023-06-05 10:47:47 +0300

    Add sorting authoring module
    * Move sorting bake logic to module
    * Improve assigning sorting layer with using `[SortingLayer]`

---
+++ Sorting/Authoring/Modules/BakerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace NSprites
+{
+    public static partial class BakerExtensions
+    {
+        /// <summary>
+        /// Bakes sorting components for NSprites-Foundation sorting system
+        /// </summary>
+        public static void BakeSpriteSorting<TAuthoring>(this Baker<TAuthoring> baker, in Entity entity, int sortingIndex, int sortingLayer, bool staticSorting = false)
+            where TAuthoring : MonoBehaviour
+        {
+            if (baker == null)
+            {
+                Debug.LogError(new NSpritesException($"Passed baker is null"));
+                return;
+            }
+
+            baker.AddComponent<VisualSortingTag>(entity);
+            baker.AddComponent<SortingValue>(entity);
+            baker.AddComponent(entity, new SortingIndex { value = sortingIndex });
+            baker.AddSharedComponent(entity, new SortingLayer { index = sortingLayer });
+            if (staticSorting)
+                baker.AddComponent<SortingStaticTag>(entity);
+        }
+    }
+}
---
+++ Sorting/Authoring/Modules/SortingAuthoringModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace NSprites
+{
+    /// <summary>
+    /// Bakes sorting components for NSprites-Foundation sorting system
+    /// </summary>
+    [Serializable]
+    public struct SortingAuthoringModule
+    {
+        [SerializeField] public bool StaticSorting;
+        [SerializeField] public int SortingIndex;
+        [SortingLayer] [SerializeField] public int SortingLayer;
+
+        public void Bake<TAuthoring>(Baker<TAuthoring> baker)
+            where TAuthoring : MonoBehaviour
+        {
+            baker.BakeSpriteSorting
+            (
+                baker.GetEntity(TransformUsageFlags.None),
+                SortingIndex,
+                UnityEngine.SortingLayer.GetLayerValueFromID(SortingLayer),
+                StaticSorting
+            );
+        }
+    }
+}
