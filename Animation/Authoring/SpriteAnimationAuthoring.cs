9c0329f9393023aeffebef0587d31d223fe708d2
TonyMax <antoshidza@mail.ru>
2023-06-05 10:59:01 +0300

    Apply changes to both authoring classes
    From this commit there are only two authoring classes:
    * `SpriteRendererAuthoring` which is just plain monobehaviour class. It uses created modules to bake entity. Baker logic just call modules bake methods
    * `SpriteAnimationAuthoring` still inherit from `SpriteRendererAuthoring` but it's no longer bound to it. In next iteration it will be not inherited and just have it's own authoring modules, because now animation authoring have confusing unused field like `Sprite` came from it's base class.

--- Animation/Authoring/SpriteAnimationAuthoring.cs
+++ Animation/Authoring/SpriteAnimationAuthoring.cs
@@ -1,9 +1,6 @@
-﻿using System.Linq;
-using Unity.Collections;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace NSprites
 {
@@ -18,146 +15,37 @@ namespace NSprites
             {
                 if(!authoring.IsValid)
                     return;
+                
+                var initialAnimData = authoring.AnimationAuthoringModule.InitialAnimationData;
+                var initialSheetUVAtlas = (float4)NSpritesUtils.GetTextureST(initialAnimData.SpriteSheet);
+                var initialFrameUVAtlas = new float4(new float2(initialSheetUVAtlas.xy / initialAnimData.FrameCount), initialSheetUVAtlas.zw);
+                
+                authoring.RegisterSpriteData.Bake(this, authoring.OverrideTextureFromSprite ? initialAnimData.SpriteSheet.texture : null);
+                authoring.AnimationAuthoringModule.Bake(this);
 
-                var entity = GetEntity(TransformUsageFlags.None);
-
-                BakeSpriteAnimation(this, entity, authoring.AnimationSet, authoring.InitialAnimationIndex);
-
-                var initialAnimData = authoring.AnimationSet.Animations.ElementAt(authoring.InitialAnimationIndex).data;
-                var initialAnimUVAtlas = (float4)NSpritesUtils.GetTextureST(initialAnimData.SpriteSheet);
-
-                BakeSpriteRender
-                (
-                    this,
-                    entity,
-                    authoring,
-                    new float4(new float2(initialAnimUVAtlas.xy / initialAnimData.FrameCount), initialAnimUVAtlas.zw),
-                    authoring._tilingAndOffset,
-                    authoring._pivot,
-                    authoring.VisualSize,
-                    flipX: authoring._flip.x,
-                    flipY: authoring._flip.y
-                );
-                if(!authoring._disableSorting)
-                    BakeSpriteSorting
-                    (
-                        this,
-                        entity,
-                        authoring._sortingIndex,
-                        authoring._sortingLayer,
-                        authoring._staticSorting
-                    );
+                authoring.RenderSettings.Bake(this, authoring, authoring.NativeSpriteSize, initialFrameUVAtlas);
+                authoring.Sorting.Bake(this);
             }
         }
-
-        [Header("Animation Data")]
-        [FormerlySerializedAs("_animationSet")] public SpriteAnimationSet AnimationSet;
-        [FormerlySerializedAs("_initialAnimationIndex")] public int InitialAnimationIndex;
-
-        public override float2 VisualSize
+        
+        [Space]
+        [SerializeField] public AnimationAuthoringModule AnimationAuthoringModule;
+        
+        protected float2 FrameSize
         {
             get
             {
-                var animationData = AnimationSet.Animations.ElementAt(InitialAnimationIndex).data;
-                return GetSpriteSize(animationData.SpriteSheet) / animationData.FrameCount;
+                var animationData = AnimationAuthoringModule.InitialAnimationData;
+                return animationData.SpriteSheet.GetSize() / animationData.FrameCount;
             }
         }
 
-        protected override bool IsValid
-        {
-            get
-            {
-                if (AnimationSet == null)
-                {
-                    Debug.LogWarning(new NSpritesException($"{nameof(AnimationSet)} is null"));
-                    return false;
-                }
+        public override float2 NativeSpriteSize => FrameSize;
 
-                if (InitialAnimationIndex >= AnimationSet.Animations.Count)
-                {
-                    Debug.LogWarning(new NSpritesException($"{nameof(InitialAnimationIndex)} can't be greater than animations count. {nameof(InitialAnimationIndex)}: {InitialAnimationIndex}, animation count: {AnimationSet.Animations.Count}"));
-                    return false;
-                }
+        protected override bool IsValid 
+            => base.IsValid && AnimationAuthoringModule.IsValid();
 
-                if (InitialAnimationIndex < 0)
-                {
-                    Debug.LogWarning(new NSpritesException($"{nameof(InitialAnimationIndex)} can't be lower 0. Currently it is {InitialAnimationIndex}"));
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
-        public static void BakeSpriteAnimation<TAuthoring>(Baker<TAuthoring> baker, in Entity entity, SpriteAnimationSet animationSet, int initialAnimationIndex = 0)
-            where TAuthoring : MonoBehaviour
-        {
-            if(baker == null)
-            {
-                Debug.LogError(new NSpritesException("Passed Baker is null"));
-                return;
-            }
-            if (animationSet == null)
-            {
-                Debug.LogError(new NSpritesException("Passed AnimationSet is null"));
-                return;
-            }
-
-            baker.DependsOn(animationSet);
-
-            if (animationSet == null)
-                return;
-
-            if (initialAnimationIndex >= animationSet.Animations.Count || initialAnimationIndex < 0)
-            {
-                Debug.LogError(new NSpritesException($"Initial animation index {initialAnimationIndex} can't be less than 0 or great/equal to animation count {animationSet.Animations.Count}"));
-                return;
-            }
-            
-            #region create animation blob asset
-            var blobBuilder = new BlobBuilder(Allocator.Temp); //can't use `using` keyword because there is extension which use this + ref
-            ref var root = ref blobBuilder.ConstructRoot<BlobArray<SpriteAnimationBlobData>>();
-            var animations = animationSet.Animations;
-            var animationArray = blobBuilder.Allocate(ref root, animations.Count);
-
-            var animIndex = 0;
-            foreach (var anim in animations)
-            {
-                var animData = anim.data;
-                var animationDuration = 0f;
-                for (int i = 0; i < animData.FrameDurations.Length; i++)
-                    animationDuration += animData.FrameDurations[i];
-
-                animationArray[animIndex] = new SpriteAnimationBlobData
-                {
-                    ID = Animator.StringToHash(anim.name),
-                    GridSize = animData.FrameCount,
-                    UVAtlas = NSpritesUtils.GetTextureST(animData.SpriteSheet),
-                    Scale2D = new float2(animData.SpriteSheet.bounds.size.x, animData.SpriteSheet.bounds.size.y),
-                    AnimationDuration = animationDuration
-                    // FrameDuration - allocate lately
-                };
-
-                var durations = blobBuilder.Allocate(ref animationArray[animIndex].FrameDurations, animData.FrameDurations.Length);
-                for (int di = 0; di < durations.Length; di++)
-                    durations[di] = animData.FrameDurations[di];
-
-                animIndex++;
-            }
-
-            var blobAssetReference = blobBuilder.CreateBlobAssetReference<BlobArray<SpriteAnimationBlobData>>(Allocator.Persistent);
-            baker.AddBlobAsset(ref blobAssetReference, out _);
-            blobBuilder.Dispose();
-            #endregion
-
-            ref var initialAnim = ref blobAssetReference.Value[initialAnimationIndex];
-
-            baker.AddComponent(entity, new AnimationSetLink { value = blobAssetReference });
-            baker.AddComponent(entity, new AnimationIndex { value = initialAnimationIndex });
-            baker.AddComponent(entity, new AnimationTimer { value = initialAnim.FrameDurations[0] });
-            baker.AddComponent<FrameIndex>(entity);
-            
-            baker.AddComponent(entity, new MainTexSTInitial { value = initialAnim.UVAtlas });
-        }
+        [ContextMenu("Set Native Size")]
+        public void SetNativeSize() => RenderSettings.Size = FrameSize;
     }
 }
--- Base/Authoring/SpriteRendererAuthoring.cs
+++ Base/Authoring/SpriteRendererAuthoring.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using NSprites.Modules;
+﻿using NSprites.Modules;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
@@ -21,215 +19,45 @@ namespace NSprites
                     return;
 
                 DependsOn(authoring);
-                DependsOn(authoring._sprite);
-                
-                this.BakeSpriteBase(authoring.RenderData);
 
-                var entity = GetEntity(TransformUsageFlags.None);
-
-                BakeSpriteRender
-                (
-                    this,
-                    entity,
-                    authoring,
-                    NSpritesUtils.GetTextureST(authoring._sprite),
-                    authoring.UVTilingAndOffset,
-                    authoring._pivot,
-                    authoring.ScaledSize,
-                    flipX: authoring._flip.x,
-                    flipY: authoring._flip.y
-                );
-                if (!authoring._disableSorting)
-                    BakeSpriteSorting
-                    (
-                        this,
-                        entity,
-                        authoring._sortingIndex,
-                        authoring._sortingLayer,
-                        authoring._staticSorting
-                    );
+                authoring.RegisterSpriteData.Bake(this, authoring.OverrideTextureFromSprite ? authoring.Sprite.texture : null);
+                authoring.RenderSettings.Bake(this, authoring, authoring.NativeSpriteSize, NSpritesUtils.GetTextureST(authoring.Sprite));
+                authoring.Sorting.Bake(this);
             }
         }
-        
-        public enum DrawMode
-        {
-            /// <summary> Sprite will be simply stretched to it's size. </summary>
-            Simple,
-            /// <summary> Sprite will be tiled depending in it's size and native size (default sprite size). </summary>
-            Tiled
-        }
 
-        [SerializeField] protected Sprite _sprite;
-        private Sprite _lastAssignedSprite;
-        [SerializeField] protected SpriteRenderData _spriteRenderData;
-        [SerializeField] protected bool _overrideSpriteTexture = true;
-        [SerializeField] protected float2 _pivot = new(.5f);
-        [SerializeField] private float2 _size;
-        [Tooltip("Prevents changing Size when Sprite changed")][SerializeField] private bool _lockSize;
-        [SerializeField] protected DrawMode _drawMode;
-        [SerializeField] private float4 _tilingAndOffset = new(1f, 1f, 0f, 0f);
-        [SerializeField] protected bool2 _flip;
+        [SerializeField] protected Sprite Sprite;
+        [SerializeField] protected RegisterSpriteAuthoringModule RegisterSpriteData;
+        [SerializeField] protected bool OverrideTextureFromSprite = true;
         
-        [Header("Sorting")]
-        [SerializeField] protected bool _disableSorting;
-        [SerializeField] protected bool _staticSorting;
-        [SerializeField] protected int _sortingIndex;
-        [SerializeField] protected int _sortingLayer;
-        [SortingLayer]
-        [SerializeField] private int _unitySortingLayer;
+        [SerializeField] protected SpriteSettingsModule RenderSettings;
+        [SerializeField] protected SortingAuthoringModule Sorting;
 
-        public static float2 GetSpriteSize(Sprite sprite) => new(sprite.bounds.size.x, sprite.bounds.size.y);
-        public virtual float2 ScaledSize => _size * new float2(transform.lossyScale.x, transform.lossyScale.y);
-        public float2 Size
-        {
-            get => _size;
-            set
-            {
-                if(_lockSize)
-                    return;
-                _size = value;
-            }
-        }
+        public float2 ScaledSize => RenderSettings.Size * new float2(transform.lossyScale.x, transform.lossyScale.y);
+
         /// <summary> "Default" sprite size which would be a size of same sprite being placed on scene as a unity's built-in SpriteRenderer. </summary>
-        public virtual float2 NativeSpriteSize => GetSpriteSize(_sprite);
-        public virtual float4 UVTilingAndOffset
-        {
-            get => _drawMode switch
-            {
-                // just return default user defined Tiling&Offset from inspector
-                DrawMode.Simple => _tilingAndOffset,
-                // while _size of sprite can be different it's UVs stay the same - in range [(0,0) ; (1,1)]
-                // so in this case we want to get ration of size to sprite NativeSize (which should be "default" sprite size depending on it's import data) and then correct Tiling part in that ratio
-                DrawMode.Tiled => new(_tilingAndOffset.xy * _size / NativeSpriteSize, _tilingAndOffset.zw),
-                
-                _ => throw new ArgumentOutOfRangeException($"{GetType().Name}.{nameof(UVTilingAndOffset)} ({nameof(SpriteRendererAuthoring)}) {gameObject.name}: can't handle draw mode {_drawMode}")
-            };
-        }
-
-        protected virtual void OnValidate()
-        {
-            if (_sprite != _lastAssignedSprite && _sprite != null)
-            {
-                _lastAssignedSprite = _sprite;
-
-                if(!_lockSize)
-                    SetNativeSize();
-            }
-        }
+        public virtual float2 NativeSpriteSize => Sprite.GetSize();
 
         [ContextMenu("Set Native Size")]
-        private void SetNativeSize() => _size = NativeSpriteSize;
-        [ContextMenu("Debug layer")]
-        private void DebugLayer() => Debug.Log(UnityEngine.SortingLayer.GetLayerValueFromID(_unitySortingLayer));
-
-        public static void BakeSpriteRender<TAuthoring>(Baker<TAuthoring> baker, in Entity entity, TAuthoring authoring, in float4 uvAtlas, in float4 uvTilingAndOffset, in float2 pivot, in float2 scale, bool flipX = false, bool flipY = false, bool add2DTransform = true)
-            where TAuthoring : MonoBehaviour
-        {
-            if (baker == null)
-            {
-                Debug.LogError(new NSpritesException($"Passed baker is null"), authoring.gameObject);
-                return;
-            }
-            if (authoring == null)
-            {
-                Debug.LogError(new NSpritesException($"Passed authoring object is null"), authoring.gameObject);
-                return;
-            }
-
-            baker.AddComponent(entity, new UVAtlas { value = uvAtlas });
-            baker.AddComponent(entity, new UVTilingAndOffset { value = uvTilingAndOffset });
-            baker.AddComponent(entity, new Pivot { value = pivot });
-            baker.AddComponent(entity, new Scale2D { value = scale });
-            baker.AddComponent(entity, new Flip { Value = new(flipX ? -1 : 0, flipY ? -1 : 0) });
-
-            if (add2DTransform)
-            {
-                baker.AddComponentObject(entity, new Transform2DRequest { sourceGameObject = authoring.gameObject });
-                baker.DependsOn(authoring.transform);
-            }
-        }
-
-        public static void BakeSpriteSorting<TAuthoring>(Baker<TAuthoring> baker, in Entity entity, int sortingIndex, int sortingLayer, bool staticSorting = false)
-            where TAuthoring : MonoBehaviour
-        {
-            if (baker == null)
-            {
-                Debug.LogError(new NSpritesException($"Passed baker is null"));
-                return;
-            }
-
-            baker.AddComponent<VisualSortingTag>(entity);
-            baker.AddComponent<SortingValue>(entity);
-            baker.AddComponent(entity, new SortingIndex { value = sortingIndex });
-            baker.AddSharedComponent(entity, new SortingLayer { index = sortingLayer });
-            if (staticSorting)
-                baker.AddComponent<SortingStaticTag>(entity);
-        }
-
-        private static readonly Dictionary<Texture, Material> OverridedMaterials = new();
-        private static readonly int MainTexPropertyID = Shader.PropertyToID("_MainTex");
-
-        protected Material GetOrCreateOverridedMaterial(Texture texture)
-        {
-            if (!OverridedMaterials.TryGetValue(texture, out var material))
-                material = CreateOverridedMaterial(texture);
-#if UNITY_EDITOR //for SubScene + domain reload
-            else if (material == null)
-            {
-                _ = OverridedMaterials.Remove(texture);
-                material = CreateOverridedMaterial(texture);
-            }
-#endif
-            return material;
-        }
-        protected Material CreateOverridedMaterial(Texture texture)
-        {
-            var material = new Material(_spriteRenderData.Material);
-            material.SetTexture(MainTexPropertyID, _sprite.texture);
-            OverridedMaterials.Add(texture, material);
-            return material;
-        }
-
-        protected virtual SpriteRenderData RenderData
-        {
-            get
-            {
-                if (_spriteRenderData.Material == null)
-                {
-                    Debug.LogException(new NSpritesException($"{nameof(_spriteRenderData.Material)} is null"), gameObject);
-                    return default;
-                }
-
-                if (_overrideSpriteTexture && _sprite != null)
-                    // create new instance with overrided material
-                    return new()
-                    {
-                        Material = GetOrCreateOverridedMaterial(_sprite.texture),
-                        PropertiesSet = _spriteRenderData.PropertiesSet
-                    };
-                return _spriteRenderData;
-            }
-        }
+        private void SetNativeSize() => RenderSettings.TrySetSize(NativeSpriteSize);
+        
+        internal void OnSpriteChanged() => SetNativeSize();
 
         protected virtual bool IsValid
         {
             get
             {
-                if (_sprite == null)
+                if (!RegisterSpriteData.IsValid(out var message))
                 {
-                    Debug.LogWarning(new NSpritesException($"{nameof(_sprite)} is null"), gameObject);
+                    Debug.LogWarning(new NSpritesException(message), gameObject);
                     return false;
                 }
+                
+                // Settings just have struct values and there is nothing to validate
 
-                if (_spriteRenderData.Material == null)
+                if (Sprite == null)
                 {
-                    Debug.LogWarning(new NSpritesException($"{nameof(SpriteRenderData.Material)} is null"), gameObject);
-                    return false;
-                }
-
-                if (_spriteRenderData.PropertiesSet == null)
-                {
-                    Debug.LogWarning(new NSpritesException($"{nameof(SpriteRenderData.PropertiesSet)} is null"), gameObject);
+                    Debug.LogWarning(new NSpritesException($"{GetType().Name}: {nameof(Sprite)} is null"), gameObject);
                     return false;
                 }
 
