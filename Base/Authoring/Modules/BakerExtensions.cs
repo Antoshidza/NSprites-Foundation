4432ec462932edda9c9c6733c3888b66686eda50
TonyMax <antoshidza@mail.ru>
2023-06-05 10:53:03 +0300

    Update BakerExtensions.cs
    Move baking components like `UVAtlas` / `UVTilingAndOffset` / `Pivot` / etc from `SpriteRendererAuthoring` to `BakerExtensions` static class

--- Base/Authoring/Modules/BakerExtensions.cs
+++ Base/Authoring/Modules/BakerExtensions.cs
@@ -1,14 +1,19 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
-namespace NSprites.Modules
+namespace NSprites
 {
     public static partial class BakerExtensions
     {
+        /// <summary>
+        /// Bakes basic sprite data. Use this method only if you using registration from this package.
+        /// If you want implement your own registration process please implement your own baking method.
+        /// </summary>
         public static void BakeSpriteBase<TAuthoring>(this Baker<TAuthoring> baker, in SpriteRenderData renderData)
             where TAuthoring : Component
         {
-            baker.DependsOn(renderData.Material);
+            //baker.DependsOn(renderData.Material);
             baker.DependsOn(renderData.PropertiesSet);
             
             var entity = baker.GetEntity(TransformUsageFlags.None);
@@ -16,6 +21,37 @@ namespace NSprites.Modules
             baker.AddComponentObject(entity, new SpriteRenderDataToRegister { data = renderData });
             // this comes from NSprites as extension method to add all required components to entity to let it be sprite rendered
             baker.AddSpriteRenderComponents(entity, renderData.ID);
+        }
+
+        /// <summary>
+        /// Bakes all passed data to make entity be able to rendered though shader from this package.
+        /// If you use another shader and you need some another data, please implement your own baking method.
+        /// </summary>
+        public static void BakeSpriteRender<TAuthoring>(this Baker<TAuthoring> baker, in Entity entity, TAuthoring authoring, in float4 uvAtlas, in float4 uvTilingAndOffset, in float2 pivot, in float2 scale, bool flipX = false, bool flipY = false, bool add2DTransform = true)
+            where TAuthoring : Component
+        {
+            if (baker == null)
+            {
+                Debug.LogError(new NSpritesException($"Passed baker is null"), authoring.gameObject);
+                return;
+            }
+            if (authoring == null)
+            {
+                Debug.LogError(new NSpritesException($"Passed authoring object is null"), authoring.gameObject);
+                return;
+            }
+
+            baker.AddComponent(entity, new UVAtlas { value = uvAtlas });
+            baker.AddComponent(entity, new UVTilingAndOffset { value = uvTilingAndOffset });
+            baker.AddComponent(entity, new Pivot { value = pivot });
+            baker.AddComponent(entity, new Scale2D { value = scale });
+            baker.AddComponent(entity, new Flip { Value = new(flipX ? -1 : 0, flipY ? -1 : 0) });
+
+            if (add2DTransform)
+            {
+                baker.AddComponentObject(entity, new Transform2DRequest { sourceGameObject = authoring.gameObject });
+                baker.DependsOn(authoring.transform);
+            }
         }
     }
 }
